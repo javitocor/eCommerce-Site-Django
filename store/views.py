f50e07f2355bd6b2b618c58dae548cf232b5519f
javitocor <javitocor@gmail.com>
2021-09-09 14:46:10 +0700

    add process order feature

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import *
 
 # Create your views here.
@@ -29,7 +30,7 @@ def cart(request):
     cartItems = order.get_cart_items
   else:
     items = []
-    order = {'get_cart_total': 0, 'get_cart_items':0}
+    order = {'get_cart_total': 0, 'get_cart_items':0, 'shipping':False}
     cartItems = order['get_cart_items']
 
   context = {'items':items, 'order':order, 'cartItems':cartItems}
@@ -43,7 +44,7 @@ def checkout(request):
     cartItems = order.get_cart_items
   else:
     items = []
-    order = {'get_cart_total': 0, 'get_cart_items':0}
+    order = {'get_cart_total': 0, 'get_cart_items':0, 'shipping':False}
     cartItems = order['get_cart_items']
 
   context = {'items': items, 'order': order, 'cartItems':cartItems}
@@ -73,3 +74,31 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			state=data['shipping']['state'],
+			zipcode=data['shipping']['zipcode'],
+			)
+	else:
+		print('User is not logged in')
+
+	return JsonResponse('Payment submitted..', safe=False)
