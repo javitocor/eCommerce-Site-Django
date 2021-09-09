e318501720eb5927804fc2a0bd60dc525a791655
javitocor <javitocor@gmail.com>
2021-09-09 15:38:15 +0700

    clean js from checkout template

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,8 +4,6 @@ for(var i = 0; i < updateBtns.length; i++) {
   updateBtns[i].addEventListener('click', function(){
     var productId = this.dataset.product
     var action = this.dataset.action
-    console.log('productId: ', productId, 'Action:', action)
-    console.log('USER:', user)
 
     if (user === 'AnonymousUser') {
       console.log('User is not authenticated')
@@ -16,7 +14,6 @@ for(var i = 0; i < updateBtns.length; i++) {
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
 
 		var url = '/update_item/'
 
